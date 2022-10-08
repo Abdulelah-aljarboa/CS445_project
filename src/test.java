83203ddf292be9fa04d01b828df0b491f3822f1c
Abdulelah-aljarboa <105386716+Abdulelah-aljarboa@users.noreply.github.com>
2022-10-08 13:44:25 +0300

    changed the regular expression

--- src/test.java
+++ src/test.java
@@ -3,7 +3,7 @@ public class test {
         // Scanner scan = new Scanner(System.in);
         // System.out.println("regex :");
         // String reg = scan.nextLine();
-        String re = "(a|bc)(ab)*";
+        String re = "(a|bc)(ab)";
         regularExpression r1 = new regularExpression(re);
         r1.REGEX_Concatenation();
         r1.REGEX_Postfix(); 
